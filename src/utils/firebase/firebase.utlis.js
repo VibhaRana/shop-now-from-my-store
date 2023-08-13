8bce8f41d2c9a7f9e6dd76172a2a33bf8b517bee
Anurag Arora <reet@Reets-MacBook-Pro.local>
2023-08-12 20:02:44 -0700

    Firebase updates error fixed

--- src/utils/firebase/firebase.utlis.js
+++ src/utils/firebase/firebase.utlis.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  signInWithRedirect,
-  signInWithPopup,
-  GoogleAuthProvider,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-} from "firebase/auth";
+import { getAuth, 
+  signInWithRedirect, 
+  signInWithPopup, 
+  GoogleAuthProvider, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut, 
+  onAuthStateChanged } from "firebase/auth";
+
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // const firebaseConfig = {
